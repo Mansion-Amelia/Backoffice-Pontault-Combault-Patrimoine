43ea11321abddf51272da790827d2603a137df33
Laura <laura.dietsch98@gmail.com>
2020-01-31 10:07:41 +0100

    collapse in backoffice

--- src/components/ListeBackoffice.vue
+++ src/components/ListeBackoffice.vue
@@ -1,89 +1,125 @@
 <template>
-  <div class="container containerBackoffice">
-	<h1>Lieux <button  @click="setActivePageBackoffice('FormAddLocation')" class="ml-1 btn btn-primary">Ajouter un lieu</button></h1>  
-    <LocationBoxBackoffice v-for="location in getBackofficeLocation" :key="location.name" :location='location'></LocationBoxBackoffice>
-	<h1>Balades <button @click="setActivePageBackoffice('FormAddWalk')" class="ml-1 btn btn-primary">Ajouter une balade</button></h1>
-    <BaladeBoxBackoffice v-for="walk in getBackofficeWalk" :key="walk.name" :walk='walk'></BaladeBoxBackoffice>
-  </div>
+    <div class="container containerBackoffice" role="tablist">
+        <b-card no-body class="mb-1">
+            <div header-tag="header" class="p-1" role="tab">
+                <div class="m-2 d-flex justify-content-between" block href="#" v-b-toggle.accordion-1 variant="info">
+                    <h1>
+                        Lieux <button @click="setActivePageBackoffice('FormAddLocation')" class="ml-1 btn btn-primary">Ajouter un lieu</button>
+                    </h1>
+                    <div class="d-flex flex-column justify-content-center">
+                        <img class="down-icon" src="../img/down.png" />
+                    </div>
+                </div>
+            </div>
+            <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+                <b-card-body>
+                    <LocationBoxBackoffice v-for="location in getBackofficeLocation" :key="location.name" :location='location'></LocationBoxBackoffice>
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+
+        <b-card no-body class="mb-1">
+            <div header-tag="header" class="p-1" role="tab">
+                <div class="m-2 d-flex justify-content-between" block href="#" v-b-toggle.accordion-2 variant="info">
+                    <h1>Balades <button @click="setActivePageBackoffice('FormAddWalk')" class="ml-1 btn btn-primary">Ajouter une balade</button></h1>
+                    <div class="d-flex flex-column justify-content-center">
+                        <img class="down-icon" src="../img/down.png" />
+                    </div>
+                </div>
+            </div>
+            <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+                <b-card-body>
+                    <BaladeBoxBackoffice v-for="walk in getBackofficeWalk" :key="walk.name" :walk='walk'></BaladeBoxBackoffice>
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+    </div>
 </template>
 
 <script>
-// RecentDocuments.vue
-import { db,storageRef } from '../config/db'
-import {mapActions, mapGetters} from 'vuex'
+    // RecentDocuments.vue
+    import { db, storageRef } from '../config/db'
+    import { mapActions, mapGetters } from 'vuex'
 
-import LocationBoxBackoffice from '../components/LocationBoxBackoffice'
-import BaladeBoxBackoffice from '../components/BaladeBoxBackoffice'
+    import LocationBoxBackoffice from '../components/LocationBoxBackoffice'
+    import BaladeBoxBackoffice from '../components/BaladeBoxBackoffice'
 
 
-import FormDeleteLocation from '../components/FormDeleteLocation'
-import FormAddLocation from '../components/FormAddLocation'
-import FormAddWalk from '../components/FormAddWalk'
-import FormDeleteWalk from '../components/FormDeleteWalk'
+    import FormDeleteLocation from '../components/FormDeleteLocation'
+    import FormAddLocation from '../components/FormAddLocation'
+    import FormAddWalk from '../components/FormAddWalk'
+    import FormDeleteWalk from '../components/FormDeleteWalk'
     export default {
-  name: "Backoffice",
-  components: {
-    FormDeleteLocation,
-    FormAddLocation,
-    FormAddWalk,
-    FormDeleteWalk,
-    LocationBoxBackoffice,
-    BaladeBoxBackoffice
-  },
-  data() {
-    return {
-      documents: [],
-      errors: [],
-      locations: [],
-      walks: []
-    }
-  },
-  firebase: {
-    documents: db.ref()
-  },
-  methods:{
-      ... mapActions([
+        name: "Backoffice",
+        components: {
+            FormDeleteLocation,
+            FormAddLocation,
+            FormAddWalk,
+            FormDeleteWalk,
+            LocationBoxBackoffice,
+            BaladeBoxBackoffice
+        },
+        data() {
+            return {
+                documents: [],
+                errors: [],
+                locations: [],
+                walks: []
+            }
+        },
+        firebase: {
+            documents: db.ref()
+        },
+        methods: {
+            ...mapActions([
                 'setActivePageBackoffice',
                 'setBackofficeLocation',
                 'setBackofficeWalk'
-        ]),
-    readLocations(){
-        let self=this
-		var query =  db.ref('app/locations/').orderByKey();
-		query.once("value")
-		.then(function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-        self.locations.push(childSnapshot.val());  
-      });
-      self.setBackofficeLocation(self.locations)
-		});
-	},
-	readWalks(){
-        let self=this
-		var query =  db.ref('app/walks/').orderByKey();
-		query.once("value")
-		.then(function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-				self.walks.push(childSnapshot.val());  
-      });
-            self.setBackofficeWalk(self.walks)
+            ]),
+            readLocations() {
+                let self = this
+                var query = db.ref('app/locations/').orderByKey();
+                query.once("value")
+                    .then(function (snapshot) {
+                        snapshot.forEach(function (childSnapshot) {
+                            self.locations.push(childSnapshot.val());
+                        });
+                        self.setBackofficeLocation(self.locations)
+                    });
+            },
+            readWalks() {
+                let self = this
+                var query = db.ref('app/walks/').orderByKey();
+                query.once("value")
+                    .then(function (snapshot) {
+                        snapshot.forEach(function (childSnapshot) {
+                            self.walks.push(childSnapshot.val());
+                        });
+                        self.setBackofficeWalk(self.walks)
 
-		});
-    },
-  
-  },
-  computed:{
-            ... mapGetters([
+                    });
+            },
+
+        },
+        computed: {
+            ...mapGetters([
                 'getBackofficeLocation',
                 'getBackofficeWalk'
             ]),
-    },
-	mounted: function(){
-		this.readLocations()
-		this.readWalks()
-	}
-}
+        },
+        mounted: function () {
+            this.readLocations()
+            this.readWalks()
+        }
+    }
 </script>
 
 <style>
- </style>
+    .down-icon {
+        height: 1.5em;
+        margin-right: 1em;
+    }
+    .containerBackoffice h1 {
+        margin-left : 1em;
+    }
+</style>
